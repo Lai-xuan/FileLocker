5840786346a582e20a47f1d96fbbb0478328507a
Lai-xuan <95519765+Lai-xuan@users.noreply.github.com>
2025-02-11 22:51:42 +0800

    Add files via upload

---
+++ app/ReadMe.txt
@@ -0,0 +1,7 @@
+本軟體不允許任何人(除本人)透過任何渠道對此軟體「FileLockerApp」(以下簡稱本軟體)進行任何交易，且不允許任何人改變本軟體內容後將本軟體透過任何方式流通、販售。
+
+-軟體製作人:lx
+
+This software does not allow anyone (except myself) to conduct any transactions with this software "FileLockerApp" (hereinafter referred to as this software) through any channel, and does not allow anyone to change the content of this software and then circulate or sell this software in any way.
+
+-Software producer:lx
---
+++ v1.0-beta/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace FileLockerApp
+{
+    public partial class App : Application
+    {
+        private static Mutex mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool isNewInstance;
+            mutex = new Mutex(true, "Global\\FileLockerApp_Mutex", out isNewInstance);
+
+            if (!isNewInstance)
+            {
+                MessageBox.Show("應用程式已經在執行中。", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+
+            // 這裡手動建立 MainWindow（確保不會開兩個視窗）
+            MainWindow mainWindow = new MainWindow();
+
+            // 如果是透過 .locked 檔案關聯開啟的
+            if (e.Args.Length > 0)
+            {
+                string filePath = e.Args[0];
+
+                if (filePath.EndsWith(".locked", StringComparison.OrdinalIgnoreCase))
+                {
+                    mainWindow.FilePathTextBox.Text = filePath;
+                }
+            }
+
+            mainWindow.Show();
+        }
+    }
+}
