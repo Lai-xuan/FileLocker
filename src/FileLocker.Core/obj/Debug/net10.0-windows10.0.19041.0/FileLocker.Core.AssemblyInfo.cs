cc97b780f96d4394d3c45d6bb4673f6c3c14d9a6
Lai-Xuan <042098.kevin@gmail.com>
2026-07-24 16:24:05 +0800

    Update FileLocker.Core and FileLocker.Core.Tests binaries and documentation
    - Updated FileLocker.Core.dll and FileLocker.Core.pdb binaries to reflect new changes.
    - Updated FileLocker.Core.AssemblyInfo.cs and FileLocker.Core.Tests.AssemblyInfo.cs with new informational version.
    - Updated sourcelink.json files for both FileLocker.Core and FileLocker.Core.Tests to point to the latest commit.
    - Updated various cache files to reflect changes in assembly references.
    - Updated technical specification document to version 1.2, including new fixes and enhancements.

--- src/FileLocker.Core/obj/Debug/net10.0-windows10.0.19041.0/FileLocker.Core.AssemblyInfo.cs
+++ src/FileLocker.Core/obj/Debug/net10.0-windows10.0.19041.0/FileLocker.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileLocker.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e542ff7ff40572240d466851a393528151bcbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098526cc3774d896dd4c289deab525d8523f75f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileLocker.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileLocker.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
